04672f31bc445bc8cf975113c0e19166d3a89cc0
Zhu Zhan Yan <program.nom@gmail.com>
2018-09-10 11:06:26 +0800

    Added optmisation functionality to imgopt

--- imgopt/img_opt.py
+++ imgopt/img_opt.py
@@ -1,11 +1,16 @@
+#!env python3
 #
 # img_opt.py
 # PIL Based Image size optimizer
 #
 
 from getopt import getopt
+from PIL import Image
+from multiprocessing import Pool, cpu_count
+
 import os
 import sys
+import io
 
 # Parse the given argument vector argv into enabled options and image path list
 # Checks if the image paths given by the user are valid.
@@ -37,15 +42,46 @@ def parse_args(argv):
     return options, image_paths
 
 
+# Optimise the given image object
+# returns an optimised BytesIO object of image
+def optimise_img(img, quality):
+    opt_img = io.BytesIO()
+    img.save(opt_img, optimize=True, quality=quality)
+    
+    return opt_img
+    
+# Apply optmisation at the image at the given path 
+# Writes the optmised image at the path onstructued by combining the given output
+# directory, the given path, and the given path suffix
+def apply_optimisation(path, quality=85, output_dir=".", suffix="", verbose=False):
+    # Apply image optmisation
+    if verbose: print("optimising {}..".format(path))
+    img = Image.open(path)
+    opt_img = optimise_img(img, quality)
+    
+    # Write optmised file to disk
+    opt_path = os.path.join(output_dir, path + suffix)
+    with open(path, "wb") as f:
+        f.write(opt_img.read())
+                        
+
 if __name__ == "__main__":
     # Determine program configuration
     USAGE = """\
-    imgopt [-q <quality>] [-s <suffix>] [-h] <paths to imgs...> 
-    -h - display this usage infomation
-    -q <quality> - the quality to preserve in the optimized image as a pecentage
-    -p <suffix>  - the suffix to add the original path to consitute the optimised path
-    -o <output dir> - output image to output directory
-    -v - verbose
-    """
+imgopt [-q <quality>] [-s <suffix>] [-h] <paths to imgs...> 
+-h - display this usage infomation
+-q <quality> - the quality to preserve in the optimized image as a pecentage
+-p <suffix>  - the suffix to add the original path to consitute the optimised path
+-o <output dir> - output image to output directory
+-v - verbose"""
     options, image_paths = parse_args(sys.argv)
-    print(options, image_paths)
+
+    if options["help"]: 
+        print(USAGE)
+        sys.exit(0)
+        
+    # Apply image optmisisation concurrently on multiple processes
+    if options["verbose"]: print("using {} processes.".format(cpu_count()))
+    processes = Pool(cpu_count())
+    apply = (lambda path: apply_optimisation(path, **options))
+    processes.map(apply, image_paths)
