fd5af6cf89cb8001aa83407ab4bf19fb0984b1f1
Zhu Zhan Yan <program.nom@gmail.com>
2018-09-10 13:38:04 +0800

    Added scale option to imgopt script

--- imgopt/img_opt.py
+++ imgopt/img_opt.py
@@ -18,88 +18,108 @@ import io
 def parse_args(argv):
     options = {
         "quality" : 85,
+        "scale" : 100,
         "output_dir": "optimized",
-        "suffix": "",
-        "help": False,
         "verbose": False,
     }
 
     # Read command line args into options
     args = argv[1:] # Skip program name
-    opts, image_paths = getopt(args, "vhq:p:o:")
+    opts, image_paths = getopt(args, "vhs:q:p:o:")
     opts = dict(opts)
     
-    if "-h" in opts: options["help"] = True
+    if "-h" in opts: 
+        USAGE = """\
+imgopt [-q <quality>] [-s <scale>] [-o <output_dir>] [-h] <paths to imgs...> 
+-h - display this usage infomation
+-q <quality> - the quality to preserve in the optimized image as a pecentage
+-s <scale> - the amount of scaling applied to the optimised image as a percentage
+-o <output dir> - output image to output directory
+-v - verbose"""
+        print(USAGE)
+        sys.exit(0)
+
     if "-v" in opts: options["verbose"] = True
     if "-q" in opts: options["quality"] = int(opts["-q"])
+    if "-s" in opts: options["scale"] = int(opts["-s"])
     if "-o" in opts: options["output_dir"] = opts["-o"]
+    
     
     # Check image paths
     for path in image_paths:
         if not os.path.exists(path): 
-            raise FileNotFoundError("Image path passed does exist")
+            print("Error: Image path passed does exist")
+            sys.exit(1)
         
     return options, image_paths
 
 
-# Optimise the given image object
+# Optimise the given image object of the given format 
+# Downsample the optmised image based on the given quality percentage
 # returns an optimised BytesIO object of image
-def optimise_img(img, quality):
+def optimise_img(img, quality, img_format):
     opt_img = io.BytesIO()
-    img.save(opt_img, format=img.format, optimize=True, quality=quality)
+    img.save(opt_img, format=img_format, optimize=True, quality=quality)
     
     return opt_img
+
+# Rescale the given image object by a factor defined by the given scale percentage
+# Returns the rescaled image object
+def rescale_img(img, scale, verbose=False):
+    factor = scale / 100
+    scaled_height = round(img.height * factor)
+    scaled_width = round(img.width * factor)
     
-# Apply optmisation at the image at the given path 
-# Writes the optmised image at the path onstructued by combining the given output
-# directory, the given path, and the given path suffix
-def apply_optimisation(path, quality=85, output_dir=".", suffix="", verbose=False):
-    # Apply image optmisation
-    if verbose: print("optimising {}..".format(path))
+    if verbose:
+        print("({},{}) -> ({},{})".format(img.width, img.height,
+                                          scaled_width, scaled_height))
+    
+    return img.resize((scaled_width, scaled_height))
+    
+    
+# Apply optimization at the image at the given path 
+# Writes the optimised image at the path constructed by combining the given output
+# directory, the given path, and the given path
+def apply_optimisation(path, quality=85, scale=100, output_dir=".", verbose=False):
     img = Image.open(path)
-    opt_img = optimise_img(img, quality)
+    img_format = img.format
+
+    # Apply image optimisation
+    if verbose: print("optimising {}..".format(path))
+    scaled_img = rescale_img(img, scale, verbose)
+    opt_img = optimise_img(scaled_img, quality, img_format)
 
     if verbose:
         print("{} => {}".format(os.path.getsize(path), opt_img.getbuffer().nbytes))
     
-    # Write optmised file to disk
-    opt_path = os.path.join(output_dir, os.path.basename(path) + suffix)
+    # Write optimised file to disk
+    opt_path = os.path.join(output_dir, os.path.basename(path))
     with open(opt_path, "wb") as f:
         f.write(opt_img.getbuffer())
                         
 
-# Apply optmisation objects
+# Apply optimisation objects
 def apply(objective):
     apply_optimisation(objective["path"],
                        objective["quality"],
+                       objective["scale"],
                        objective["output_dir"],
-                       objective["suffix"],
                        objective["verbose"])
     
 if __name__ == "__main__":
     # Determine program configuration
-    USAGE = """\
-imgopt [-q <quality>] [-s <suffix>] [-h] <paths to imgs...> 
--h - display this usage infomation
--q <quality> - the quality to preserve in the optimized image as a pecentage
--p <suffix>  - the suffix to add the original path to consitute the optimised path
--o <output dir> - output image to output directory
--v - verbose"""
     options, image_paths = parse_args(sys.argv)
 
-    if options["help"]: 
-        print(USAGE)
-        sys.exit(0)
-
+    # Create output directory if it does already exists 
     if not os.path.exists(options["output_dir"]):
         os.mkdir(options["output_dir"])
         
-    # Construct optmisation objectives (path + options)
+    # Construct optimisation objectives (path + options)
     objectives = [ dict(options) for p in image_paths ]
     for objective, path in zip(objectives, image_paths):
         objective["path"] = path
         
-    # Apply image optmisisation concurrently on multiple processes
+    # Apply image optimisation concurrently on multiple processes
     if options["verbose"]: print("using {} processes.".format(cpu_count()))
     processes = Pool(cpu_count())
     processes.map(apply, objectives)
